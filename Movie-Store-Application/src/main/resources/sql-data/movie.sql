f04102e72e06abbaa364e6dcd392edde1f8ec6f4
Piotr Dancewicz <s28113@pjwstk.edu.pl>
2025-01-04 20:24:54 +0100

    Added more movies

--- Movie-Store-Application/src/main/resources/sql-data/movie.sql
+++ Movie-Store-Application/src/main/resources/sql-data/movie.sql
@@ -7,4 +7,20 @@ values
     ('Christopher Nolan', 'Dunkirk', '19.99', 'true', 'Dunkirk is a 2017 war film that depicts the Dunkirk evacuation of World War II. Its ensemble cast includes Fionn Whitehead, Tom Glynn-Carney, Jack Lowden, Harry Styles, Cillian Murphy, Mark Rylance, and Tom Hardy.'),
     ('Denis Villeneuve', 'Bladerunner 2049', '39.99', 'true', 'Blade Runner 2049 is a 2017 science fiction film. A sequel to the 1982 film Blade Runner, the film stars Ryan Gosling and Harrison Ford, with Ana de Armas and others.'),
     ('Denis Villeneuve', 'Arrival', '29.99', 'false', 'Arrival is a 2016 American science fiction film. It is based on the 1998 short story "Story of Your Life" by Ted Chiang and stars Amy Adams, Jeremy Renner, and Forest Whitaker.'),
-    ('Denis Villeneuve', 'Dune', '69.99', 'true', 'Dune is 2020 American science film based on book written by Frank Herbert "Dune". It features stars like Zendeya, Jason Mamoa, Timothee Chamalet and Rebecca Ferguson');
+    ('Denis Villeneuve', 'Dune', '69.99', 'true', 'Dune is 2020 American science film based on book written by Frank Herbert "Dune". It features stars like Zendeya, Jason Mamoa, Timothee Chamalet and Rebecca Ferguson'),
+    ('Denis Villeneuve', 'Sicario', '19.99', 'true', 'Sicario is a 2015 American action thriller film. It stars Emily Blunt, Benicio del Toro, and Josh Brolin.'),
+    ('Steven Spielberg', 'Jurassic Park', '29.99', 'false', 'Jurassic Park is a 1993 science fiction film. It is the first installment of the Jurassic Park franchise and is based on the 1990 novel of the same name by Michael Crichton.'),
+    ('George Lucas', 'Star Wars: A New Hope', '19.99', 'true', 'Star Wars is a 1977 American epic space-opera film. It was written and directed by George Lucas.'),
+    ('George Lucas', 'Star Wars: The Empire Strikes Back', '19.99', 'true', 'The Empire Strikes Back is a 1980 American epic space opera film directed by Irvin Kershner.'),
+    ('George Lucas', 'Star Wars: Return of the Jedi', '19.99', 'true', 'Return of the Jedi is a 1983 American epic space opera film directed by Richard Marquand.'),
+    ('George Lucas', 'Star Wars: The Phantom Menace', '19.99', 'false', 'The Phantom Menace is a 1999 American epic space-opera film written and directed by George Lucas.'),
+    ('George Lucas', 'Star Wars: Attack of the Clones', '19.99', 'true', 'Attack of the Clones is a 2002 American epic space-opera film directed by George Lucas.'),
+    ('George Lucas', 'Star Wars: Revenge of the Sith', '19.99', 'true', 'Revenge of the Sith is a 2005 American epic space-opera film written and directed by George Lucas.'),
+    ('George Lucas', 'Star Wars: The Force Awakens', '19.99', 'true', 'The Force Awakens is a 2015 American epic space-opera film produced, co-written, and directed by J. J. Abrams.'),
+    ('George Lucas', 'Star Wars: The Last Jedi', '19.99', 'true', 'The Last Jedi is a 2017 American epic space-opera film written and directed by Rian Johnson.'),
+    ('George Lucas', 'Star Wars: The Rise of Skywalker', '19.99', 'true', 'The Rise of Skywalker is a 2019 American epic space-opera film produced, co-written, and directed by J. J. Abrams.'),
+    ('George Lucas', 'Star Wars: Rogue One', '19.99', 'true', 'Rogue One is a 2016 American epic space-opera film directed by Gareth Edwards.'),
+    ('George Lucas', 'Star Wars: Solo', '19.99', 'true', 'Solo is a 2018 American epic space-opera film directed by Ron Howard.'),
+    ('George Lucas', 'Star Wars: The Bad Batch', '19.99', 'true', 'The Bad Batch is a 2021 American animated series created by Dave Filoni for the streaming service Disney+. It is part of the Star Wars franchise, acting as both a sequel to and spin-off from the series Star Wars: The Clone Wars.'),
+    ('George Lucas', 'Star Wars: The Book of Boba Fett', '19.99', 'true', 'The Book of Boba Fett is an upcoming American television series created for the streaming service Disney+. It is part of the Star Wars franchise, set to premiere on December 29, 2021.');
+
